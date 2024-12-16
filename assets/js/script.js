2fb316adfbbc7189eea4b32ff0f57ab3a3d8654c
SujalCodeCraft <sujal920501@gmail.com>
2024-12-16 17:27:25 +0530

    fully responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,9 @@ $(document).ready(function () {
             0: {
                 items: 1        // 1 item on small screens
             },
-            600: {
-                items: 2        // 2 items on medium screens
+            576: {
+                items: 2  ,
+                center:false      // 2 items on medium screens
             },
             1000: {
                 items: 3     // 3 items on large screens
